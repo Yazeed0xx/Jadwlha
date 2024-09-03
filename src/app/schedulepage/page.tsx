fec634e9411e9c6bfc8637b1dc7110b1449ff9a9
Yazeed0xx <yazeedthunyan@gmail.com>
2024-09-03 05:19:32 +0300

    Fixed type error

--- src/app/schedulepage/page.tsx
+++ src/app/schedulepage/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React, { useState } from 'react'
 import dynamic from 'next/dynamic';
-import UserForm from '@/components/Form';
 
 const Google = dynamic(() => import('../../components/Google'), { ssr: false });
 
