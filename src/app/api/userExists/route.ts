710ed7ec97ab3f55b477f560f5b67278051b1f33
Yazeed0xx <yazeedthunyan@gmail.com>
2024-09-04 05:48:47 +0300

    fixed

--- src/app/api/userExists/route.ts
+++ src/app/api/userExists/route.ts
@@ -8,12 +8,10 @@ export async function POST(req: NextRequest) {
     const reqBody = await req.json();
     const { email } = reqBody;
 
-    // Querying the database to check if the user exists
     const user2 = await User.findOne({ email }).select("_id");
 
-    console.log("User:", user2); // Logging the user data for debugging
+    console.log("User:", user2); 
     
-    // Returning a JSON response with the user data (or null if not found)
     return NextResponse.json({ user2 });
   } catch (error) {
     console.log("Error:", error);
